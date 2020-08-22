9e38758fe8d3cceb4291d3f938eb0bde8d12bfd3
Farzon Lotfi <farzonl@icloud.com>
2020-08-22 19:27:31 -0400

    add a small bit of instruction decode logic for XED, builds does not run, investigate libc-dynamic.dylib issue

---
+++ plugins/xedPlugin/test/test.cpp
@@ -0,0 +1,19 @@
+
+#define CATCH_CONFIG_MAIN
+
+#include <catch2/catch.hpp>
+#include "xedDisassembler.h"
+
+TEST_CASE( "test 2 instructions" ) {
+    unsigned char assembly[] = "\x55\x48\x8b\x05\xb8\x13\x00\x00";
+    XedDisassembler disasm(Archtype::X86_64);
+    disasm.Decode(assembly, sizeof(assembly)-1);
+
+}
+
+//TEST_CASE( "test more instructions" ) {
+//    unsigned char assembly[] = "\x55\x31\xd2\x89\xe5\x8b\x45\x08\x56\x8b\x75\x0c\x53\x8d\x58\xff\x0f\xb6\x0c\x16\x88\x4c\x13\x01\x83\xc2\x01\x84\xc9\x75\xf1\x5b\x5e\x5d\xc3";
+//    XedDisassembler disasm(Archtype::X86_64);
+//    disasm.Decode(assembly, sizeof(assembly)-1);
+//    std::cout << disasm << std::endl;
+//}
--- plugins/xedPlugin/xedDisassembler.h
+++ plugins/xedPlugin/xedDisassembler.h
@@ -3,11 +3,16 @@
 
 #include "interfaceDisassembler.h"
 
+struct XedInternal;
+
 class XedDisassembler : public AbstractDisassembler {
     public:
         XedDisassembler(Archtype archType);
         virtual ~XedDisassembler();
         virtual void Decode(const unsigned char *code, int size) final;
+    private:
+        int decodeInstruction(const unsigned char *code, int size);
+        std::unique_ptr<XedInternal> xedInternal;
 };
 
 #endif // __Xed_Disassembler_H__
